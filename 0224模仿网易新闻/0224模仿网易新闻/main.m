837fc1617b63393ff714d829f1707716f2b75b04
wangqingyan <wangqingyan222@gmail.com>
2016-02-24 18:57:24 +0800

    初始化项目

---
+++ 0224模仿网易新闻/0224模仿网易新闻/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  0224模仿网易新闻
+//
+//  Created by 王庆岩 on 16/2/24.
+//  Copyright © 2016年 王庆岩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ 0224模仿网易新闻/0224模仿网易新闻Tests/_224______Tests.m
@@ -0,0 +1,39 @@
+//
+//  _224______Tests.m
+//  0224模仿网易新闻Tests
+//
+//  Created by 王庆岩 on 16/2/24.
+//  Copyright © 2016年 王庆岩. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _224______Tests : XCTestCase
+
+@end
+
+@implementation _224______Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
