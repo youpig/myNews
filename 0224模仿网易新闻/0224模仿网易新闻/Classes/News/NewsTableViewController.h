91b90ac99f7659118e101c00945dca931bfcb097
wangqingyan <wangqingyan222@gmail.com>
2016-02-24 19:11:36 +0800

    搭建项目框架

---
+++ 0224模仿网易新闻/0224模仿网易新闻/Classes/News/NewsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewsTableViewController.h
+//  0224模仿网易新闻
+//
+//  Created by 王庆岩 on 16/2/24.
+//  Copyright © 2016年 王庆岩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsTableViewController : UITableViewController
+
+@end
--- 0224模仿网易新闻/0224模仿网易新闻/Classes/News/NewsTableViewController.m
+++ 0224模仿网易新闻/0224模仿网易新闻/Classes/News/NewsTableViewController.m
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  NewsTableViewController.m
 //  0224模仿网易新闻
 //
 //  Created by 王庆岩 on 16/2/24.
 //  Copyright © 2016年 王庆岩. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "NewsTableViewController.h"
 
-@interface ViewController : UIViewController
-
+@implementation NewsTableViewController
 
 @end
-
