819365573a0a54621ec6cbadd15550eea610d939
wangqingyan <wangqingyan222@gmail.com>
2016-02-24 23:54:16 +0800

    设置了缓存 设置了网络指示器

---
+++ 0224模仿网易新闻/0224模仿网易新闻/Classes/News/News.h
@@ -0,0 +1,13 @@
+//
+//  News.h
+//  0224模仿网易新闻
+//
+//  Created by 王庆岩 on 16/2/24.
+//  Copyright © 2016年 王庆岩. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface News : NSObject
+
+@end
---
+++ 0224模仿网易新闻/0224模仿网易新闻/Classes/News/News.m
@@ -0,0 +1,13 @@
+//
+//  News.m
+//  0224模仿网易新闻
+//
+//  Created by 王庆岩 on 16/2/24.
+//  Copyright © 2016年 王庆岩. All rights reserved.
+//
+
+#import "News.h"
+
+@implementation News
+
+@end
