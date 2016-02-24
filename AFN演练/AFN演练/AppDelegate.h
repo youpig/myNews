0a24a83233303828a8bc54b801538c5170aafb07
wangqingyan <wangqingyan222@gmail.com>
2016-02-24 22:32:08 +0800

    添加afn框架

---
+++ AFN演练/AFN演练/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFN演练
+//
+//  Created by 王庆岩 on 16/2/24.
+//  Copyright © 2016年 王庆岩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
